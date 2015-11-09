2cc13660e2c7ace51c08593a3e999ca0fcf69dbb
EdieShimel <edieshimel@gmail.com>
2015-11-08 21:58:25 -0500

    Creates Outlines table, migrates table, checks rails db

--- storWe_app_api/db/schema.rb
+++ storWe_app_api/db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151109023910) do
+ActiveRecord::Schema.define(version: 20151109025417) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "outlines", force: :cascade do |t|
+    t.string   "subjects"
+    t.string   "settings"
+    t.string   "themes"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",           null: false
